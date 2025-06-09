9b6644189de0aa265ff54fefec173c9f0bf9139a
hungdv36 <hungdvph48701@gmail.com>
2025-06-09 17:02:46 +0700

    ..

---
+++ database/migrations/2025_06_09_093820_create_coupons_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 50)->unique()->nullable();
+            $table->enum('discount_type', ['percent', 'fixed'])->nullable();
+            $table->decimal('discount_value', 10, 2)->nullable();
+            $table->integer('max_usage')->nullable();
+            $table->integer('usage_count')->default(0);
+            $table->dateTime('start_date')->nullable();
+            $table->dateTime('end_date')->nullable();
+            $table->dateTime('created_at')->nullable();
+            $table->dateTime('updated_at')->nullable();
+        });
+    }
+
+    public function down(): void {
+        Schema::dropIfExists('coupons');
+    }
+};
