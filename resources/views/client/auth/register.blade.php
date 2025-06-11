9bc981dddcaa5c0abe9e6bcfb8ac9575d6c73e1c
TRANTHI <cuthi090@gmail.com>
2025-06-11 20:17:50 +0700

    RegiesterRegiester

--- resources/views/client/auth/register.blade.php
+++ resources/views/client/auth/register.blade.php
@@ -49,15 +49,7 @@
                             <p>Tạo tài khoản</p>
                         </div>
                         <div class="login-box">
-                            {{-- @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                            @endif --}}
+
 
                             <form method="POST" action="{{ route('register') }}" class="row g-3">
                                 @csrf
