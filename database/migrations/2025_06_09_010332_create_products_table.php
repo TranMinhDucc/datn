ad8cf2e5f8795cd29e1aa9e4644d5fc1c6f0fad3
Tran Minh Duc <ttuongtacsale@gmail.com>
2025-06-16 21:33:44 +0700

    update migration products

--- database/migrations/2025_06_09_010332_create_products_table.php
+++ database/migrations/2025_06_09_010332_create_products_table.php
@@ -1,13 +1,14 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
-    public function up(): void {
+    public function up(): void
+    {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('category_id')->nullable();
             $table->unsignedBigInteger('brand_id')->nullable();
 
@@ -27,13 +28,13 @@ return new class extends Migration {
             $table->timestamps();
 
             // Foreign keys
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
             $table->foreign('brand_id')->references('id')->on('brands')->onDelete('set null');
         });
     }
 
-    public function down(): void {
+    public function down(): void
+    {
         Schema::dropIfExists('products');
     }
 };
