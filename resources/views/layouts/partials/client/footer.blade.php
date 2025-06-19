e7893ee166771f408f2be6f02cffb464d09e0d9b
NguyenNguyenVu <vucbcb@gamil.com>
2025-06-19 14:20:58 +0700

    update sửa lại tồn kho create

--- resources/views/layouts/partials/client/footer.blade.php
+++ resources/views/layouts/partials/client/footer.blade.php
@@ -13,7 +13,7 @@
 
                           <ul>
                               <li> <i class="iconsax" data-icon="location"></i>
-                                  <h6>{{ $settings['address']}}</h6>
+                                  <p>{{ $settings['address'] ?? 'Chưa cập nhật địa chỉ' }}</p>
                               </li>
                               <li> <i class="iconsax" data-icon="phone-calling"></i>
                                   <h6>{{ $settings['hotline']}}</h6>
